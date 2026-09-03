3dbe1897b76a125933a7367e1b4330c9a3964a7f
zhanghuaze <625198513@qq.com>
2026-09-03 10:03:49 +0800

    v1.0 driverlib版本

---
+++ FLASH/device/subdir_rules.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+device/%.obj: ../device/%.asm $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'C2000 Compiler - building file: "$<"'
+	"C:/ti/ccs2100/ccs/tools/compiler/ti-cgt-c2000_25.11.1.LTS/bin/cl2000" -v28 -ml -mt --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --vcu_support=vcu2 -O2 --include_path="C:/ti/c2000/C2000Ware_26_01_00_00/driverlib/f2837xd/driverlib" --include_path="C:/Users/62519/workspace_ccstheia/28377d_driverlib_PRJ/device" --include_path="C:/Users/62519/workspace_ccstheia/28377d_driverlib_PRJ" --include_path="C:/ti/ccs2100/ccs/tools/compiler/ti-cgt-c2000_25.11.1.LTS/include" --define=_FLASH --define=CPU1 --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="device/$(basename $(<F)).d_raw" --obj_directory="device" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+device/%.obj: ../device/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'C2000 Compiler - building file: "$<"'
+	"C:/ti/ccs2100/ccs/tools/compiler/ti-cgt-c2000_25.11.1.LTS/bin/cl2000" -v28 -ml -mt --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --vcu_support=vcu2 -O2 --include_path="C:/ti/c2000/C2000Ware_26_01_00_00/driverlib/f2837xd/driverlib" --include_path="C:/Users/62519/workspace_ccstheia/28377d_driverlib_PRJ/device" --include_path="C:/Users/62519/workspace_ccstheia/28377d_driverlib_PRJ" --include_path="C:/ti/ccs2100/ccs/tools/compiler/ti-cgt-c2000_25.11.1.LTS/include" --define=_FLASH --define=CPU1 --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="device/$(basename $(<F)).d_raw" --obj_directory="device" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
---
+++ main.c
@@ -0,0 +1,60 @@
+#include "device.h"
+#include "driverlib.h"
+
+void main(void)
+{
+    //
+    // Initialize device clock and peripherals
+    //
+    Device_init();
+
+    //
+    // Initialize GPIO and configure the GPIO pin as a push-pull output
+    //
+    Device_initGPIO();
+    GPIO_setPadConfig(DEVICE_GPIO_PIN_LED2, GPIO_PIN_TYPE_STD);
+    GPIO_setDirectionMode(DEVICE_GPIO_PIN_LED2, GPIO_DIR_MODE_OUT);
+
+    //
+    // Initialize PIE and clear PIE registers. Disables CPU interrupts.
+    //
+    Interrupt_initModule();
+
+    //
+    // Initialize the PIE vector table with pointers to the shell Interrupt
+    // Service Routines (ISR).
+    //
+    Interrupt_initVectorTable();
+
+    //
+    // Enable Global Interrupt (INTM) and realtime interrupt (DBGM)
+    //
+    EINT;
+    ERTM;
+
+    //
+    // Loop Forever
+    //
+    for(;;)
+    {
+        //
+        // Turn on LED
+        //
+        GPIO_writePin(DEVICE_GPIO_PIN_LED2, 0);
+
+        //
+        // Delay for a bit.
+        //
+        DEVICE_DELAY_US(500000);
+
+        //
+        // Turn off LED
+        //
+        GPIO_writePin(DEVICE_GPIO_PIN_LED2, 1);
+
+        //
+        // Delay for a bit.
+        //
+        DEVICE_DELAY_US(500000);
+    }
+}
